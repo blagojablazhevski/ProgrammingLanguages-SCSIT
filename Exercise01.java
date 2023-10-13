0883aab03e296f9c63a3eb49df42fa5f3aca220c
blagojablazhevski <128937678+blagojablazhevski@users.noreply.github.com>
2023-10-13 17:55:31 +0200

    Update Exercise01.java

--- Exercise01.java
+++ Exercise01.java
@@ -1,3 +1,4 @@
+
 class myNumber{
 	private int num;
 	public int num2;
@@ -5,24 +6,32 @@ class myNumber{
 		num = 0;
 		num2 = 100;
 	}
-		
 	int getNum() {
 		return num;
 	}
-		
+	int getNum2() {
+		return num2;
+	}
+	void description() {
+		System.out.println("Num: " + num + " Num2: " + num2);
+	}
 	void setNum(int i) {
 		num = i;
 		num2 = 10 * i;
+	}
+	public myNumber(int _num, int _num2) {
+		num = _num;
+		num2 = _num2;
+	}
+	float average() {
+		return (num+num2)/2;
 	}
 }
 
 public class exercisesClass {
 	public static void main(String[] args) {
-		myNumber A = new myNumber();
-		System.out.println(A.getNum());
-		A.setNum(10);
-		System.out.println(A.getNum());
-		System.out.println(A.num2);
+		myNumber A = new myNumber(13, 10);
+		A.description();
+		System.out.println("The average of these two numbers is: " + A.average());
 	}
-
 }
