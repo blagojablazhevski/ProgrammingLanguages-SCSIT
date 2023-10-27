bda218a4af4d3fc0da8319abf48c46cf8c6fea86
blagojablazhevski <128937678+blagojablazhevski@users.noreply.github.com>
2023-10-27 18:19:09 +0200

    Update Exercise02.java

--- Lecture05/Exercise02.java
+++ Lecture05/Exercise02.java
@@ -1,7 +1,14 @@
+
 public class Exercise02 {
 	
 	static class egg{
-		 public void EGG() {
+		int category;
+		
+		public egg(int category) {
+			this.category = category;
+		}
+		
+		public void EGG() {
 			System.out.println("Each egg has an albumen and a yolk");
 		}
 		public void print() {
@@ -10,14 +17,18 @@ public class Exercise02 {
 	}
 	
 	static class ostrich_egg extends egg{
+		public ostrich_egg(int category) {
+			super(category);
+			System.out.println("a constructor of ostrich_egg that utilizies the constructor of eggg");
+		}
 		public void EGG() {
 			System.out.println("The albumen and yolk an OSTRICH egg are the largest");
 		}
 	}
 
 	public static void main(String[] args) {
-		egg henneg = new egg();
-		ostrich_egg ostrichegg = new ostrich_egg();
+		egg henneg = new egg(2);
+		ostrich_egg ostrichegg = new ostrich_egg(4);
 		
 		henneg.EGG();
 		ostrichegg.EGG();
