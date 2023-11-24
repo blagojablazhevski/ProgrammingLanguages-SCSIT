6608eb074e1eaedee66182902b1a5320a87f395f
Blagoja Blazhevski <128937678+blagojablazhevski@users.noreply.github.com>
2023-11-24 18:08:57 +0100

    Create Test.java

---
+++ Lecture08/Assignment01/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+	String s;
+	
+	public Test(){
+		s = "Programming Languages";
+	}
+	
+	public void method1() {
+		System.out.println(s);
+	}
+	
+	public void method2(int n) {
+		System.out.println(n);
+	}
+	
+	private void method3() {
+		System.out.println("Private method is invoked");
+	}
+}
