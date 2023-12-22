eb41880711f378b78fa6e299d115d4ecf2ec58dc
Blagoja Blazhevski <128937678+blagojablazhevski@users.noreply.github.com>
2023-12-22 18:51:16 +0100

    Update Assignment1.java

--- Lecture12Exceptions/Assignment1.java
+++ Lecture12Exceptions/Assignment1.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 public class Assignment1 {
 
 	public static void main(String[] args) {
-		try (// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		try (
 		Scanner input = new Scanner(System.in)) {
 			System.out.print("What is your name? ");
 			String name = input.nextLine();
